9cf9b7f8e18ecfd849927b3f2289ddf39489950f
rpdevries <141464209+rpdevries@users.noreply.github.com>
2024-01-29 11:01:44 -0600

    Build folder small items

---
+++ Build/FossilScope.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:58feb4d3b5cbefc8ece19ab3da9dd39d5f60e59adc91928ad6e38c019612229e
+size 518042
---
+++ Build/FossilScope.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4a03b2823f93d1deccf3a8e008243e9048804b4f8129f38cfe744bd514923f8
+size 13311
